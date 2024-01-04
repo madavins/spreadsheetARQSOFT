eeabff9fa54c8655625e95f086ec0052895ad88b
madavins <maneldavins@gmail.com>
2024-01-04 20:12:17 +0100

    Adding code to functions. Will depend on how cells are excatly defined.

--- spreadsheet/src/main/java/spreadsheet/Average.java
+++ spreadsheet/src/main/java/spreadsheet/Average.java
@@ -15,7 +15,26 @@ public class Average implements Function{
     }
     
     public float calculate(){
-        return 0;
+        float sum = 0;
+        int count = 0;
+        for(Argument arg : this.args){
+            if(arg instanceof Function){
+                sum += ((Function)arg).calculate();
+                count++;
+            }else if(arg instanceof Number){
+                sum += ((Number)arg).getValue();
+                count++;
+            }else if(arg instanceof Cell){
+                //Com es tracta una cela?
+                continue;
+            }else if(arg instanceof Range){
+                for(Cell c : ((Range)arg).getRange()){
+                    //Com es tracta una cela?
+                    continue;
+                }
+            }
+        }
+        return sum/count;
     }
     
 }
--- spreadsheet/src/main/java/spreadsheet/Max.java
+++ spreadsheet/src/main/java/spreadsheet/Max.java
@@ -15,7 +15,30 @@ public class Max implements Function{
     }
     
     public float calculate(){
-        return 0;
+        float max = 0;
+        for(Argument arg : this.args){
+            if(arg instanceof Function){
+                float aux = ((Function)arg).calculate();
+                if(aux > max){
+                    max = aux;
+                }
+            }else if(arg instanceof Number){
+                float aux = ((Number)arg).getValue();
+                if(aux > max){
+                    max = aux;
+                }
+            }else if(arg instanceof Cell){
+                //Com es tracta una cela?
+                continue;
+            }else if(arg instanceof Range){
+                for(Cell c : ((Range)arg).getRange()){
+                    //Com es tracta una cela?
+                    continue;
+                }
+            }
+        }
+        
+        return max;
     }
     
 }
